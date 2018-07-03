ff0c0dfcfed47df2b4ab17ca3120f302d971b47c
Angelo De Laurentis <angelodelaurentis@Angelos-MacBook-Air.local>
2018-07-03 16:15:27 -0700

    committed

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World',
+        'tester': 123,
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
